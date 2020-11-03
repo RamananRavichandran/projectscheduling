e4b0071900ce0c43327de59bf39bea2dda6de1a3
RamananRavichandran <gr.ramanan2010@gmail.com>
2020-11-03 15:28:42 +0530

    made changes to server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import render_template
 import connexion
+import os
 
 
 # Create the application instance
@@ -21,4 +22,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
